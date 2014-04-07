9a74347532c02f0a037daa2f83c026c3db5e6621
Simon Uyttendaele <simon.uyttendaele@gmail.com>
2014-04-07 10:01:23 +0200

    Create esri.symbol.MultiLineTextSymbol.js

---
+++ esri.symbol.MultiLineTextSymbol.js
@@ -0,0 +1,38 @@
+require(["esri/symbols/TextSymbol", "dojox.gfx.svg.Text"], function(ts, t)
+{
+	if( typeof dojox.gfx.svg.Text.prototype.setShape == 'function' )
+	{
+		dojox.gfx.svg.Text.prototype.setShape = function(p)
+		{
+			this.shape = dojox.gfx.makeParameters(this.shape, p);
+			this.bbox = null;
+			var r = this.rawNode, s = this.shape;
+			r.setAttribute("x", s.x);
+			r.setAttribute("y", s.y);
+			r.setAttribute("text-anchor", s.align);
+			r.setAttribute("text-decoration", s.decoration);
+			r.setAttribute("rotate", s.rotated ? 90 : 0);
+			r.setAttribute("kerning", s.kerning ? "auto" : 0);
+			r.setAttribute("text-rendering", "optimizeLegibility");
+			
+			while(r.firstChild)
+				r.removeChild(r.firstChild);
+
+			if(s.text)
+			{ 
+				var texts = s.text.split("\n");
+				var lineHeight = 1.1 * parseInt(document.defaultView.getComputedStyle(r, "").getPropertyValue("font-size"), 10); 
+				for(var i = 0, n = texts.length; i < n; i++)
+				{ 
+					var tspan = (document.createElementNS ? document.createElementNS(dojox.gfx.svg.xmlns.svg, "tspan") : document.createElement("tspan"));
+					tspan.setAttribute("dy", i ? lineHeight : -(texts.length-1)*lineHeight/2); 
+					tspan.setAttribute("x", s.x);
+					tspan.appendChild((dojox.gfx.useSvgWeb ? document.createTextNode(texts[i], true) : document.createTextNode(texts[i]))); 
+					r.appendChild(tspan);
+				}
+			}
+
+			return this;
+		}
+	}
+});
